9620ea949a4d9b061ef8bbb7580ab89f0abbb25e
kuladog <2coffees1banana@gmail.com>
2021-02-10 06:17:37 -0800

    updated

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Python Blackjack!
 
-1) Player can buy any amount of chips, game over when 0 chips
-2) Dealer uses four decks and builds a new stack at 25 cards
-3) Dealer must hit when less than 17, stand if 17 or more
-4) Ace follows soft rules, values 11 and 1, auto blackjack
-5) Blackjack pays out 3:2 or * 1.5 of bet
-6) Bank eats the float if less than one chip
-7) There's no splitting, doubling down, or insurance (yet)
+- Player can buy any amount of chips, game over when 0 chips
+- Dealer uses four decks and builds a new stack at 25 cards
+- Dealer must hit when less than 17 or stand if 17 or more
+- Ace follows soft rules, values 11 and 1, auto blackjack
+- Blackjack pays out 3:2 or * 1.5 of bet
+- Bank eats the float if less than one chip
+- There's no splitting, doubling down, or insurance (yet)
 
--- blackjack.py
+++ blackjack.py
@@ -1,16 +1,5 @@
 #!/bin/python
 
-"""
-*) Python Blackjack!
-1) Player can buy any amount of chips, game over when 0 chips
-2) Dealer uses four decks and builds a new stack at 25 cards
-3) Dealer must hit when less than 17, stand if 17 or more
-4) Ace follows soft rules, values 11 and 1, auto blackjack
-5) Blackjack pays out 3:2 or * 1.5 of bet
-6) Bank eats the float if less than one chip
-7) There's no splitting, doubling down, or insurance (yet)
-"""
-
 import random
 import time
 import subprocess
@@ -104,7 +93,7 @@ class Bank:
         self.chips += self.bet * 1.5
 
     def eat_float(self):
-        if self.chips < 1: 
+        if self.chips < 1:
             self.chips = int(self.chips)
             return self.chips
 
@@ -150,7 +139,7 @@ def display():
 
 
 def wager():
-    bank.eat_float() 
+    bank.eat_float()
     print("\nYou have " + str(bank.chips) + " chips!")
     if bank.chips <= 0:
         time.sleep(0.8)
